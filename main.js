8d0e3ccd2dd0353e3858f12c4ce0ab2abe65bf60
Farhod Gofurov <fg325609@gmail.com>
2026-04-14 16:55:17 +0500

    kjfkjjjf

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const wrapper = document.getElementById("container");
 
-fetch('https://jsonplaceholder.typicode.com/comments')
+fetch('https://jsonplaceholder.typicode.com/photos')
       .then(response => response.json())
       .then(data =>{
         render(data)
@@ -10,9 +10,9 @@ fetch('https://jsonplaceholder.typicode.com/comments')
         const cards = users.map(el =>`
              <div class="card">
                   <img src="https://picsum.photos/id/${el.id + 10}/600/200" alt="food">
-                   <h1>${el.name}</h1>
-                    <p>@${el.email}</p>
-                   <a href="mailto:${el.body}">${el.email}</a>
+                   <h1>${el.title}</h1>
+                    <p>@${el.url}</p>
+                   <a href="mailto:${el.thumbnailUrl}">${el.thumbnailUrl}</a>
                 </div>
             `).join("");
             wrapper.innerHTML = cards;
